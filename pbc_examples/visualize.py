a24ed5777dd38a2c041efc52fd94a37b359c79cc
GituserAbi <abigogreen@gmail.com>
2026-06-30 12:34:49 +0530

    added some visualization changes

--- pbc_examples/visualize.py
+++ pbc_examples/visualize.py
@@ -19,21 +19,16 @@ def exact_u(Exact, x, t, nu, beta, rho, layers, N_f, L, source, u0_str, system, 
     cbar = fig.colorbar(h, cax=cax)
     cbar.ax.tick_params(labelsize=15)
 
-    line = np.linspace(x.min(), x.max(), 2)[:,None]
-
     ax.set_xlabel('t', fontweight='bold', size=30)
     ax.set_ylabel('x', fontweight='bold', size=30)
-    ax.legend(
-        loc='upper center',
-        bbox_to_anchor=(0.9, -0.05),
-        ncol=5,
-        frameon=False,
-        prop={'size': 15}
-    )
 
     ax.tick_params(labelsize=15)
 
-    plt.savefig(f"{path}/exactu_{system}_nu{nu}_beta{beta}_rho{rho}_Nf{N_f}_{layers}_L{L}_source{source}_{u0_str}.pdf")
+    plt.savefig(
+        f"{path}/exactu_{system}_nu{nu}_beta{beta}_rho{rho}_Nf{N_f}_{layers}_L{L}_source{source}_{u0_str}.pdf",
+        bbox_inches='tight',
+        dpi=300
+    )
     plt.close()
 
     return None
@@ -57,22 +52,17 @@ def u_diff(Exact, U_pred, x, t, nu, beta, rho, seed, layers, N_f, L, source, lr,
     cbar = fig.colorbar(h, cax=cax)
     cbar.ax.tick_params(labelsize=15)
 
-    line = np.linspace(x.min(), x.max(), 2)[:,None]
-
     ax.set_xlabel('t', fontweight='bold', size=30)
     ax.set_ylabel('x', fontweight='bold', size=30)
 
-    ax.legend(
-        loc='upper center',
-        bbox_to_anchor=(0.9, -0.05),
-        ncol=5,
-        frameon=False,
-        prop={'size': 15}
-    )
-
     ax.tick_params(labelsize=15)
 
-    plt.savefig(f"{path}/udiff_{system}_nu{nu}_beta{beta}_rho{rho}_Nf{N_f}_{layers}_L{L}_seed{seed}_source{source}_{u0_str}_lr{lr}.pdf")
+    plt.savefig(
+        f"{path}/udiff_{system}_nu{nu}_beta{beta}_rho{rho}_Nf{N_f}_{layers}_L{L}_seed{seed}_source{source}_{u0_str}_lr{lr}.pdf",
+        bbox_inches='tight',
+        dpi=300
+    )
+    plt.close()
 
     return None
 
@@ -91,22 +81,16 @@ def u_predict(u_vals, U_pred, x, t, nu, beta, rho, seed, layers, N_f, L, source,
     cbar = fig.colorbar(h, cax=cax)
     cbar.ax.tick_params(labelsize=15)
 
-    line = np.linspace(x.min(), x.max(), 2)[:,None]
-
     ax.set_xlabel('t', fontweight='bold', size=30)
     ax.set_ylabel('x', fontweight='bold', size=30)
 
-    ax.legend(
-        loc='upper center',
-        bbox_to_anchor=(0.9, -0.05),
-        ncol=5,
-        frameon=False,
-        prop={'size': 15}
-    )
-
     ax.tick_params(labelsize=15)
 
-    plt.savefig(f"{path}/upredicted_{system}_nu{nu}_beta{beta}_rho{rho}_Nf{N_f}_{layers}_L{L}_seed{seed}_source{source}_{u0_str}_lr{lr}.pdf")
+    plt.savefig(
+        f"{path}/upredicted_{system}_nu{nu}_beta{beta}_rho{rho}_Nf{N_f}_{layers}_L{L}_seed{seed}_source{source}_{u0_str}_lr{lr}.pdf",
+        bbox_inches='tight',
+        dpi=300
+    )
 
     plt.close()
     return None
